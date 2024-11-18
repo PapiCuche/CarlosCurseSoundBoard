c9fd390435718c9ba2164a9364fc9de82e56ebf3
PapiCuche <carlos.curse@tecsup.edu.pe>
2024-11-18 03:13:41 -0500

    Initial Commit

---
+++ CarlosCurseSoundBoardTests/CarlosCurseSoundBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarlosCurseSoundBoardTests.swift
+//  CarlosCurseSoundBoardTests
+//
+//  Created by Mauricio Curse on 18/11/24.
+//
+
+import Testing
+@testable import CarlosCurseSoundBoard
+
+struct CarlosCurseSoundBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
